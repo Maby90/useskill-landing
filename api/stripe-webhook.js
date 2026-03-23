4fad9b81413e7f82e6a69c803bb1ff4e48498bd5
Maby90 <hello@mprochilo.it>
2026-03-23 20:57:07 +0100

    feat: gruppo MailerLite Content Calendar

--- api/stripe-webhook.js
+++ api/stripe-webhook.js
@@ -8,6 +8,7 @@ const PRODUCT_GROUPS = {
   'linkedin-post-writer': '182758422813344940',
   'instagram-carousel':   '182759842155333516',
   'newsletter-generator': '182762133059012306',
+  'content-calendar':     '182762270881744685',
 }
 
 async function addToMailerLite(email, groupId) {
